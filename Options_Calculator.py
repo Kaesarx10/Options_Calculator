21058b87a9bb1b8226a245827468b1869a068825
kaesarx10 <venzorcesar@gmail.com>
2024-10-09 13:27:55 -0500

    # added initial def for black-scholes

--- Options_Calculator.py
+++ Options_Calculator.py
@@ -1,1 +1,9 @@
-#Simple Options Calculator 1
+#Simple Options Calculator One
+
+import numpy as np
+from scipy.stats import norm
+
+# Need to define Black-Scholes formula for calculating the price of an Option Security.
+# Will need variables for Current Stock Price, Years to Maturity, Risk Free Interest Rate, Volitality of underlying Security, Strike Price, and a string Variable for Option Type.
+
+def black_scholes(S, K, T, r, sigma, option_type):
