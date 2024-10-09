1628509d3c41f8750c9785cc84bcf1957561aac1
kaesarx10 <venzorcesar@gmail.com>
2024-10-09 16:29:10 -0500

    Added d2 explanation and formula.

--- Options_Calculator.py
+++ Options_Calculator.py
@@ -23,9 +23,16 @@ def black_scholes(S, K, T, r, sigma, option_type):
 	start_time = time.time()
 
 # Before calculation the Complete Formula where price C = S*N(d1)-K*e^(-rT) * N(d2), d1 and d2 are defined
-# d1 represents the normalized distance between the current stock price anf the strike price, adjusted for the time value of money and the volatility of the security
+# d1 represents the normalized distance between the current stock price and the strike price, adjusted for the time value of money and the volatility of the security
 # Growth rate is incoporated through rate r, and volatility incorporated through sigma.
 # This is the probability the Option will be in the money at the expiration, adjusted for time value of money. 
 # d1 = [ ln(S/K) + (r+(sigma^2)/2) * T ] / [ sigma * T^(1/2) ]
 
 	d1 = (np.log(S/K) + (r + .5*sigma**2) * T ) / (sigma * np.sqrt(T))
+
+# d2 is derived by subtracting the product sigmaand the square root of T, from d1. 
+# d2 = d1 - sigma * T^(1/2)
+# This is used to calculate the probability that the option will be excercised, considering the discounting effect of the risk-free rate.
+
+	d2 = d1 - sigma * np.sqrt(T)
+
