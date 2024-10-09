8b4815158fbfc13d278df0e8e4525b6e9133ccc4
kaesarx10 <venzorcesar@gmail.com>
2024-10-09 13:37:55 -0500

    Added variable to def and added timer for computaional time measurement.

--- Options_Calculator.py
+++ Options_Calculator.py
@@ -7,3 +7,16 @@ from scipy.stats import norm
 # Will need variables for Current Stock Price, Years to Maturity, Risk Free Interest Rate, Volitality of underlying Security, Strike Price, and a string Variable for Option Type.
 
 def black_scholes(S, K, T, r, sigma, option_type):
+	Parameters:
+	S (float): Current Stock Price
+	K (float): Option Strike Price
+	T (float): Time to Maturity (in Yrs)
+	r (float): Rist-Free Interst Rate
+	sigma (float): Volatility of the underlying security (annual)
+	option_type (str): 'call', or 'put'
+
+	Returns:
+	tuple: Option Price and Computation Time in Seconds
+	
+# must start timer to obtain computaional time.
+	start_time = time.time()
