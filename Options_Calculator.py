9c0d773813fa0577a7638e93ecd6ac5be5385332
kaesarx10 <venzorcesar@gmail.com>
2024-10-10 23:07:46 -0500

    Added clarification on pricing formula function.

--- Options_Calculator.py
+++ Options_Calculator.py
@@ -1,4 +1,4 @@
-#Simple Options Calculator One
+#Simple European Style Options Calculator One
 
 import time
 import numpy as np
@@ -22,17 +22,23 @@ def black_scholes(S, K, T, r, sigma, option_type):
 # must start timer to obtain computaional time.
 	start_time = time.time()
 
-# Before calculation the Complete Formula where price C = S*N(d1)-K*e^(-rT) * N(d2), d1 and d2 are defined
+# Before calculating the Complete Formula for price of either a Call, or a Put, through; 
+
+#		C = S * N(d1) - K * e^(-rT) * N(d2)
+#		P = K * e^(-rT) * N(-d2) - S * N(-d1)
+
+# d1 and d2 are defined. 
+
 # d1 represents the normalized distance between the current stock price and the strike price, adjusted for the time value of money and the volatility of the security
 # Growth rate is incoporated through rate r, and volatility incorporated through sigma.
-# This is the probability the Option will be in the money at the expiration, adjusted for time value of money. 
+# This isused to calculate the probability the Option will be In-the-Money at the expiration, adjusted for time value of money. 
 # d1 = [ ln(S/K) + (r+(sigma^2)/2) * T ] / [ sigma * T^(1/2) ]
 
 	d1 = (np.log(S/K) + (r + .5*sigma**2) * T ) / (sigma * np.sqrt(T))
 
 # d2 is derived by subtracting the product sigmaand the square root of T, from d1. 
 # d2 = d1 - sigma * T^(1/2)
-# This is used to calculate the probability that the option will be excercised, considering the discounting effect of the risk-free rate.
+# This is used to calculate the probability that the Option will be Excercised, considering the discounting effect of the risk-free rate.
 
 	d2 = d1 - sigma * np.sqrt(T)
 
